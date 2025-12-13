e4701adb559dd36504e5ecc2ee03ca52656463a8
LeightonJime <jimevaalau@gmail.com>
2025-12-12 22:10:34 -0600

    prueba tablón de anuncios

--- proyectoCS/articulos/articulos.php
+++ proyectoCS/articulos/articulos.php
@@ -109,7 +109,7 @@ $totalLibros = ($resultadoLibros) ? $resultadoLibros->num_rows : 0;
                   <li class="menu-item"><a href="../index.html#popular-books" class="nav-link">Populares</a></li>
                   <li class="menu-item"><a href="../index.html#special-offer" class="nav-link">Ofertas</a></li>
                   <li class="menu-item"><a href="../index.html#latest-blog" class="nav-link">Libros</a></li>
-                  <li class="menu-item"><a href="../index.html#download-app" class="nav-link">Descargar App</a></li>
+                  <li class="menu-item"><a href="../libros/Anuncio.html" class="nav-link">Anuncios</a></li>
 
                   <!-- Página actual -->
                   <li class="menu-item active">
